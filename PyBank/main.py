141003d7ba285dbe2024ed8759c04ee6f6becf3a
paullbrown <120353565+paullbrown@users.noreply.github.com>
2023-01-17 15:00:53 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,9 +7,36 @@ budget_data_df = pd.read_csv(budget_data_csv_path, encoding="utf8")
 budget_data_df.head()
 
 
-total_months = budget_data_df["Months"].value_counts()
-total_months
+totalmonths = len(budget_data_df['Date'])
+print(totalmonths)
+
+sumprofit = budget_data_df['Profit/Losses'].sum()
+print(sumprofit)
+
+change_df = budget_data_df['Profit/Losses'].diff()
+print(change_df)
 
 
-net_df = budget_data_df.sum("Profit/Losses", int)
-net_df
+avgchange_df = change_df.mean()
+print(avgchange_df)
+
+
+max_change_df = change_df.max()
+min_change_df = change_df.min()
+
+print(max_change_df)
+print(min_change_df)
+print('Financial Analysis')
+print()
+print('-----------------------')
+print()
+print('Total Months: ' + str(totalmonths))
+print()
+print('Total: ' + str(sumprofit))
+print()
+print('Average Change: ' + str(avgchange_df))
+print()
+print('Greatest Increase in Profits: ' + str(max_change_df))
+print()
+print('Greatest Decrease in Profits: ' +str( min_change_df)) 
+
